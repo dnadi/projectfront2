733e055132aee233061f8bdfccd7d3df676f61ba
dnadi <dumby651@gmail.com>
2024-02-01 18:51:22 +0200

    started login click

--- main.js
+++ main.js
@@ -7,3 +7,14 @@
       console.log(error);
   });
 })();
+
+
+function login(){
+  axios.get("http://localhost8000/login")
+  .then((response) => {
+    window.location.href = "http://localhost:5173/home";
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+}
